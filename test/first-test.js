a05fd4ae88c200e3b489e7029bb65eaf8c1462cb
Aaron Evans <aarone@one-shore.com>
2020-11-20 08:21:51 -0700

    add first test

---
+++ test/first-test.js
@@ -0,0 +1,9 @@
+let expect = require('chai').expect;
+
+describe("Hogwarts", function() {
+
+    it("should admits wizards", function() {
+        let wizard = true;
+        expect(wizard).to.be.true;
+    });
+});
