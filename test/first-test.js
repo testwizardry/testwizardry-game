b3cdf7bcfadd9eb122138d15a16fead5010ba0b7
fijiaaron <fijiaaron@users.noreply.github.com>
2020-11-20 15:40:24 +0000

    deploy: c83127347a77f83560e1717cbb329542c4970ae8

--- test/first-test.js
+++
@@ -1,9 +0,0 @@
-let expect = require('chai').expect;
-
-describe("Hogwarts", function() {
-
-    it("should admits wizards", function() {
-        let wizard = true;
-        expect(wizard).to.be.true;
-    });
-});
